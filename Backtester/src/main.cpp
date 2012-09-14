73120336d7ec86276b9dcb194110b9cb97ecb1da
Jeffrey Mazar <jeffrey.mazar@gmail.com>
2012-09-13 17:35:50 -0700

    Added sqlite and database skeleton files.

--- Backtester/src/main.cpp
+++ Backtester/src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/simulator.h"
 #include "../include/investor.h"
+#include "../3rdparty/sqlite/sqlite3.h"
 
 int main() {
     Simulator   simulation;
@@ -9,5 +10,13 @@ int main() {
     simulation.AddInvestor(investor);
     simulation.RunSimulation();
 
+		sqlite3 * db;
+		int rc;
+		rc = sqlite3_open("test.db" ,&db);
+		if(rc) {
+			printf("That shit cray\n");
+			sqlite3_close(db);
+		}
+		sqlite3_close(db);
     return 0;
 }

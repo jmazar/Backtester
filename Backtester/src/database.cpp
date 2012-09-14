e56934ebbb4af9521615d11e325eccda30a34f5e
jmazar <jeffrey.mazar@gmail.com>
2012-09-14 13:26:43 -0700

    Added second table for securities.

--- Backtester/src/database.cpp
+++ Backtester/src/database.cpp
@@ -8,8 +8,10 @@ Database::Database() {
 	}
 	sqlite3_stmt * statement;
 
-	int rc = sqlite3_prepare_v2(m_pDatabase, "CREATE TABLE IF NOT EXISTS security_names ( id INT AUTO_INCREMENT, name TEXT);", -1, &statement, 0);
+	int rc = sqlite3_prepare_v2(m_pDatabase, "CREATE TABLE IF NOT EXISTS security_names ( id INTEGER PRIMARY KEY, name TEXT);", -1, &statement, 0);
+	printf("Return id %d\n", rc);
 
+	rc = sqlite3_prepare_v2(m_pDatabase, "CREATE TABLE IF NOT EXISTS securities ( id INTEGER PRIMARY KEY, name_id INTEGER, date TEXT, open REAL, close REAL, high REAL, low REAL, volume INTEGER);", -1, &statement, 0);
 	printf("Return id %d\n", rc);
 }
 
